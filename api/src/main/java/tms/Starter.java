e712cf33900cd7e5942913469d6fefb4211f4c67
KarenTorosyan <karen.torosyan@outlook.com>
2023-12-07 21:26:04 +0400

    Gradle app config

---
+++ api/src/main/java/tms/Starter.java
@@ -0,0 +1,13 @@
+package tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class Starter {
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
+}
---
+++ api/src/test/java/tms/StarterTest.java
@@ -0,0 +1,15 @@
+package tms;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@DisplayName("Starter should")
+@ActiveProfiles("test")
+public class StarterTest {
+
+    @DisplayName("load app context")
+    @Test
+    void shouldLoadAppContext() {
+    }
+}
